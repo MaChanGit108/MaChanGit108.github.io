fcc0c3004780cb751be5a17602f08f30cdcb72c6
MaChanGit108 <50186087+MaChanGit108@users.noreply.github.com>
2020-10-21 00:05:13 -0400

    Added random func and fixed some typo

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,9 @@
 // You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function range(int) {
   const arr = [];
@@ -32,24 +37,25 @@ document.body.addEventListener('submit', async (e) => {
       if (document.querySelector('.flex-inner')) {
         document.querySelector('.flex-inner').remove();
       }
-      const newArr = range(20);
+      const newArr = range(10);
       const newArr2 = newArr.map(() => {
         const number = getRandomIntInclusive(0, 243);
         return fromServer(number);
       });
-      const reverseList = newArr2.sort((a,b) => sortFunction(b, a, 'name'));
-      const ul = document.createElement('ul');
+      
+      const reverseList = newArr2.sort((a, b) => sortFunction(b, a, 'name'));
+      const ul = document.createElement('ol');
       ul.className = 'flex-inner';
       $('form').prepend(ul);
 
       reverseList.forEach((el, i) => {
-        const li = document.createEelement('li');
+        const li = document.createElement('li');
         $(li).appened(`<input type="checkbox" value=${el.code} id=${el.code} />`);
         $(li).append(`<label for=${el.code}>${el.code}</label>`);
         $(ul).append(li);
       });
 
-      console.log('fromServer', fromServer);
+      // console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
 });
